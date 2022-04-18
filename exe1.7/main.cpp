5e0809d6756d6c0793d50669227e2050a4795df9
jakob-a2k <jakub.a2k@gmail.com>
2022-04-18 18:25:52 +0200

    std::cout i cout

---
+++ exe1.7/main.cpp
@@ -0,0 +1,24 @@
+//sposob rozwiazania wywodzacy sie z jezyka C
+
+#include <iostream>
+
+int main()
+{
+    std::cout << "Znajdz dwa rozne rozwiazania tego zadania.";
+    return 0;
+}
+
+
+
+//sposob rozwiazania wywodzacy sie z jezyka C++
+
+/*#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    cout << "Zndz dwa rozne rozwiazania tego zadania." << endl;
+    return 0;
+}
+*/

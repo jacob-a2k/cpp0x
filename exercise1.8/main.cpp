0b0ea16d8a623b34a7671f25ff5a021c33756782
jakob-a2k <jakub.a2k@gmail.com>
2022-04-18 18:36:57 +0200

    deklaracja zmiennych i sposob ich uzycia

---
+++ exercise1.8/main.cpp
@@ -0,0 +1,75 @@
+
+//sprawdzenie pojemnosci okreslonego typu danych na moim komputerze
+
+/*
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    cout << "sizeof(bool) = " << sizeof( bool ) <<endl;
+    cout << "sizeof(char) = " << sizeof( char ) <<endl;
+    cout << "sizeof(unsigned char) = " << sizeof( unsigned char) <<endl;
+    cout << "sizeof(wchar_t) = " << sizeof(wchar_t) <<endl;
+    cout << "sizeof(short) = " << sizeof( short ) <<endl;
+    cout << "sizeof(unsigned short) = " << sizeof( unsigned short ) <<endl;
+    cout << "sizeof(int) = " << sizeof( int ) <<endl;
+    cout << "sizeof(unsigned int) = " << sizeof( unsigned int ) << endl;
+    cout << "sizeof(long) = " << sizeof( long ) <<endl;
+    cout << "sizeof(unsigned long) = " << sizeof( unsigned long ) <<endl;
+    cout << "sizeof(long long) = " << sizeof( long long ) <<endl;
+    cout << "sizeof(float) = " << sizeof( float ) <<endl;
+    cout << "sizeof(double) = " << sizeof( double ) << endl;
+    cout << "sizeof(long double) = " << sizeof( long double ) <<endl;
+    return 0;
+}
+*/
+
+
+//deklaracja zminnych i sposob ich uzyia
+/*#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int to_jest_zmienna;
+    unsigned int tylko_dodatnie;
+
+    float zmiennoprzecinkowa;
+
+    char jeden_znak;
+    unsigned char rowniez_znak;
+
+    int abc = -53;
+
+    tylko_dodatnie = 22;
+    zmiennoprzecinkowa = 12.42;
+    rowniez_znak = 'c';
+
+    cout << "Wypisujemy zmienne: " << endl;
+    cout << " to jest zmienna = " << to_jest_zmienna << endl;
+    cout << " tylko dodatnie = " << tylko_dodatnie << endl;
+    cout << " abc = " << abc << endl;
+    cout << " ziennoprzecinkowa = " << zmiennoprzecinkowa << endl;
+    cout << " jeden_znak = " << jeden_znak << endl;
+    cout << " rowniez_znak = " << rowniez_znak << endl;
+    return 0;
+}
+*/
+
+
+// Zdanie domowe!
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int nieznana = 123;
+
+    cout << "Liczba = " << nieznana << endl;
+    return 0;
+}

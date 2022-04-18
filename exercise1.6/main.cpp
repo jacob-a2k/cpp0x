c4e033463d236315358dacb7af6e0fbd958234d1
jakob-a2k <jakub.a2k@gmail.com>
2022-04-18 18:35:28 +0200

    spcja, tabulacja, now linia

---
+++ exercise1.6/main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <cstdio>
+
+int main()
+{
+    std::cout << "==============" << std::endl;
+    std::cout << "= To proste! =" << std::endl;
+    std::cout << "==============" << std::endl;
+    printf(" \t A jak to zrobic?");
+    std::cout << "\nKoniec ;)";
+    return 0;
+}
